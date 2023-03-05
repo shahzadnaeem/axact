0342d6a0a954bb9eac852ab5f0f0b2177b348a09
Shahzad Naeem <shahzad65naeem@gmail.com>
2023-03-05 17:05:32 +0000

    Track web sockets. Add reader for received messages.

--- src/index.mjs
+++ src/index.mjs
@@ -5,18 +5,23 @@ const html = htm.bind(h);
 
 function App(props) {
   return html`
-    <div>
+    <main>
+      <h3>
+        WS #${props.wsId} - Update #${props.wsEvents} - ${props.wsCount} Web
+        Sockets
+      </h3>
       ${props.cpus.map((cpu) => {
-        return html`<div class="bar">
-          <div class="bar-inner" style="width: ${cpu}%"></div>
-          <label>${cpu.toFixed(2)}%</label>
+        return html`<div class="cpu-info">
+          <div class="cpu-num">${cpu[0] + 1}</div>
+          <div class="bar">
+            <div class="bar-inner" style="width: ${cpu[1]}%"></div>
+            <label>${cpu[1].toFixed(2)}%</label>
+          </div>
         </div>`;
       })}
-    </div>
+    </main>
   `;
 }
-
-let i = 0;
 
 // let update = async () => {
 //   let response = await fetch("/api/cpus");
@@ -32,12 +37,20 @@ let i = 0;
 // setInterval(update, 200);
 
 let url = new URL("/realtime/cpus", window.location.href);
-// http => ws
-// https => wss
 url.protocol = url.protocol.replace("http", "ws");
+
+let ws_events = 0;
 
 let ws = new WebSocket(url.href);
 ws.onmessage = (ev) => {
   let json = JSON.parse(ev.data);
-  render(html`<${App} cpus=${json}></${App}>`, document.body);
+
+  ws_events++;
+
+  render(
+    html`<${App} cpus=${json.cpu_data} wsCount=${json.ws_count} wsId=${json.ws_id} wsEvents=${ws_events}></${App}>`,
+    document.body
+  );
+
+  ws.send(`WS #${json.ws_id} - Event #${ws_events}`);
 };
--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,24 @@ use axum::{
     routing::get,
     Router, Server,
 };
+use futures::{
+    sink::SinkExt,
+    stream::{SplitStream, StreamExt},
+};
+use serde::Serialize;
+use std::sync::{Arc, Mutex};
 use sysinfo::{CpuExt, System, SystemExt};
 use tokio::sync::broadcast;
 
-type Snapshot = Vec<f32>;
+#[derive(Clone, Debug, Serialize)]
+struct WsData {
+    ws_count: u32,
+    ws_total_count: u32,
+    ws_id: u32,
+    cpu_data: Vec<(usize, f32)>,
+}
+
+type Snapshot = WsData;
 
 #[tokio::main]
 async fn main() {
@@ -19,7 +33,13 @@ async fn main() {
 
     tracing_subscriber::fmt::init();
 
-    let app_state = AppState { tx: tx.clone() };
+    let app_state = AppState {
+        tx: tx.clone(),
+        ws_count: Arc::new(Mutex::new(0u32)),
+        ws_total_count: Arc::new(Mutex::new(0u32)),
+    };
+
+    // let mut_app_state = Arc::new(Mutex::new(app_state));
 
     let router = Router::new()
         .route("/", get(root_get))
@@ -33,15 +53,28 @@ async fn main() {
         let mut sys = System::new();
         loop {
             sys.refresh_cpu();
-            let v: Vec<_> = sys.cpus().iter().map(|cpu| cpu.cpu_usage()).collect();
-            let _ = tx.send(v);
+            let v: Vec<_> = sys
+                .cpus()
+                .iter()
+                .enumerate()
+                .map(|cpu| (cpu.0, cpu.1.cpu_usage()))
+                .collect();
+
+            let data = WsData {
+                ws_id: 0,
+                ws_count: *app_state.ws_count.lock().unwrap(),
+                ws_total_count: 0,
+                cpu_data: v,
+            };
+
+            let _ = tx.send(data);
             std::thread::sleep(System::MINIMUM_CPU_UPDATE_INTERVAL);
         }
     });
 
     let server = Server::bind(&"0.0.0.0:7032".parse().unwrap()).serve(router.into_make_service());
     let addr = server.local_addr();
-    println!("Listening on {addr}");
+    println!("Listening on http://{addr} ...");
 
     server.await.unwrap();
 }
@@ -49,6 +82,8 @@ async fn main() {
 #[derive(Clone)]
 struct AppState {
     tx: broadcast::Sender<Snapshot>,
+    ws_count: Arc<Mutex<u32>>,
+    ws_total_count: Arc<Mutex<u32>>,
 }
 
 #[axum::debug_handler]
@@ -83,15 +118,56 @@ async fn realtime_cpus_get(
     ws: WebSocketUpgrade,
     State(state): State<AppState>,
 ) -> impl IntoResponse {
-    ws.on_upgrade(|ws: WebSocket| async { realtime_cpus_stream(state, ws).await })
+    let id = {
+        let mut ws_count = state.ws_count.lock().unwrap();
+        *ws_count += 1u32;
+
+        let mut ws_total_count = state.ws_total_count.lock().unwrap();
+        *ws_total_count += 1u32;
+
+        *ws_total_count
+    };
+
+    ws.on_upgrade(move |ws: WebSocket| async move { realtime_cpus_stream(state, id, ws).await })
 }
 
-async fn realtime_cpus_stream(app_state: AppState, mut ws: WebSocket) {
-    let mut rx = app_state.tx.subscribe();
+async fn realtime_cpus_stream(app_state: AppState, id: u32, ws: WebSocket) {
+    let (mut sender, receiver) = ws.split();
 
-    while let Ok(msg) = rx.recv().await {
-        ws.send(Message::Text(serde_json::to_string(&msg).unwrap()))
-            .await
-            .unwrap();
+    let cloned_app_state = app_state.clone();
+
+    tokio::spawn(socket_reader(app_state, receiver));
+
+    let mut rx = cloned_app_state.tx.subscribe();
+    while let Ok(mut msg) = rx.recv().await {
+        msg.ws_id = id;
+
+        let res = sender
+            .send(Message::Text(serde_json::to_string(&msg).unwrap()))
+            .await;
+
+        match res {
+            Ok(_good) => {}
+            Err(msg) => {
+                eprintln!("WS done {:?}", msg);
+                break;
+            }
+        }
     }
+}
+
+async fn socket_reader(app_state: AppState, mut ws: SplitStream<WebSocket>) {
+    while let Some(res) = ws.next().await {
+        if let Ok(msg) = res {
+            eprintln!("Got: {:?}", msg);
+        } else {
+            eprintln!("Got: Error!");
+        }
+    }
+
+    eprintln!("Done receiving");
+
+    // We are done receiving as socket has closed
+    let mut ws_count = app_state.ws_count.lock().unwrap();
+    *ws_count -= 1u32;
 }
