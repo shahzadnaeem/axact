46cb0721cb0806b0ae787a655689bcf162ee1f6d
Shahzad Naeem <shahzad65naeem@gmail.com>
2023-03-15 22:08:51 +0000

    sysinfo version bump. New screenshot

--- src/lib/data_gen.rs
+++ src/lib/data_gen.rs
@@ -15,6 +15,24 @@ pub fn cpu_data_gen(app_state: AppState, broadcast_tx: broadcast::Sender<Snapsho
         if !have_users(&app_state) {
             println!("No users, sleeping for 1s");
             std::thread::sleep(Duration::from_secs(1));
+
+            // FIXME: This is correct, but seems to prevent detection of final client close
+            //        until a new client connects - handlers.rs:54
+            //
+            //        -- Client ID #1 closed
+            //        WS STARTED for: ID #1
+            //        No users, sleeping for 1s
+            //        No users, sleeping for 1s
+            //        No users, sleeping for 1s
+            //        No users, sleeping for 1s
+            //        No users, sleeping for 1s
+            //        No users, sleeping for 1s
+            //        No users, sleeping for 1s
+            //        WS STARTED for: ID #2
+            //        -- This only appears after new Client ID #2 starts
+            //        WS DONE for: ID #1
+
+            continue;
         }
 
         let ws_data = get_ws_data(&app_state, &mut sys);
