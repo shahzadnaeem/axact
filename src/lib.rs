d32f50b21e34fbecb060fde03fe4fc00bf6eca20
Shahzad Naeem <shahzad65naeem@gmail.com>
2024-03-06 10:57:01 +0000

    Missed refactor

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,8 @@ pub mod v2;
 #[path = "lib/data.rs"]
 pub mod data;
 
-// #[path = "lib/app_state.rs"]
-// pub mod app_state;
+#[path = "lib/data_gen.rs"]
+pub mod data_gen;
 
 #[path = "lib/handlers.rs"]
 pub mod handlers;
-
-#[path = "lib/data_gen.rs"]
-pub mod data_gen;
